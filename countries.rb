f3677851070def8ef3fb6db9b67b0ec84e9575f7
Amol Brid <amolbrid@gmail.com>
2013-09-07 11:00:53 -0400

    Enable CORS

--- countries.rb
+++ countries.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+configure do
+  enable :cross_origin
+end
+
+set :allow_origin, :any
+
 get '/countries.json' do
   content_type 'application/json'
   File.open('countries.json', 'r').readlines
